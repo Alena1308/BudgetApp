b0d78e5c91fed780061596b5aac027fb28397ac8
Alena1308 <Lenamironova13@mail.ru>
2023-01-09 09:08:38 +0300

    first

--- src/main/java/me/martynova/budgetapp/controllers/FirstController.java
+++ src/main/java/me/martynova/budgetapp/controllers/FirstController.java
@@ -1,2 +1,21 @@
-package me.martynova.budgetapp.controllers;public class FirstController {
+package me.martynova.budgetapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    @GetMapping
+    public String helloWorld(){
+        return "Приложение запущено!";
+    }
+    @GetMapping("/path/to/info")
+    public String page(@RequestParam String info){
+        return "Информация: " +
+                "Имя ученика: Мартынова А.В. ** \n" +
+                "Название проекта: BudgetApp ** \n" +
+                "Дата создания проекта: 09.01.2023 ** \n" +
+                "Первая работа над веб приложением \n" + info;
+    }
 }
